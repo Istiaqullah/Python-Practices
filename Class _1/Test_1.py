25e699a0c5e374a9c53a5e188dcf52610efbcc7c
Abul Hasnat Mohammad Istiaqullah <mohammadistiaqullah@gmail.com>
2025-11-17 18:30:31 +0600

    Some Data Types

---
+++ Class _1/Test_1.py
@@ -0,0 +1,9 @@
+print("Hello World")
+print("-------------------")
+a=1        #int
+b=2.2      #double
+c=2.0      #double
+d='x'      #char
+e="y"      #String
+f=True     #bool
+print(a,b,c,d,e,f)
