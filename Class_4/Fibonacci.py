345131a43e9a40e8e4b7e39054f137d73e02e346
Abul Hasnat Mohammad Istiaqullah <mohammadistiaqullah@gmail.com>
2025-12-05 20:58:20 +0600

    Find a number is fibonacci or not

--- Class_4/Fibonacci.py
+++ Class_4/Fibonacci.py
@@ -4,11 +4,7 @@ def is_fibonacci(n, a=0, b=1):
         return True
     if a > n:
         return False
-
-    # Tail-recursive call
     return is_fibonacci(n, b, a + b)
 
-
-# Example
 num = int(input("Enter a number: "))
 print("Fibonacci" if is_fibonacci(num) else "Not Fibonacci")
